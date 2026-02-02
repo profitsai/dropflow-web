ba3678b2480e081bcf46d66e92cb03a0a0ee70f9
profitsai <siamlegacy7@gmail.com>
2026-02-03 00:46:08 +1100

    Trigger Railway redeploy - add server startup log

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+console.log('🚀 Starting DropFlow Express Server...');
+
 // Serve static files from dist directory
 app.use(express.static(path.join(__dirname, 'dist')));
 
